3a357aadeac1b19b06b7b230d96074cdceb9a410
Vítězslav Dvořák <info@vitexsoftware.cz>
2017-09-25 18:25:59 +0200

    Services class added

---
+++ src/IPEXB2B/Services.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * IPEXB2B - Client for Access to IPEX Rights class.
+ *
+ * @author     Vítězslav Dvořák <vitex@arachne.cz>
+ * @copyright  (C) 2017 Spoje.Net
+ */
+
+namespace IPEXB2B;
+
+/**
+ * Základní třída pro čtení z IPEX
+ *
+ * @url https://restapi.ipex.cz/documentation#/
+ */
+class Services extends ApiClient
+{
+    /**
+     * Sekce užitá objektem.
+     * Section used by object
+     *
+     * @var string
+     */
+    public $section = 'services';
+
+}
